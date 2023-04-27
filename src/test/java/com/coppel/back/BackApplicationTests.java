ef0fc7b179c5957ca8a86f80f8d470c6f8e282c8
JavyDaniel <danieljavy@gmail.com>
2023-04-27 14:10:55 -0600

    creacion del proyecto y configuracion de dependencias

---
+++ src/test/java/com/coppel/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coppel.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
